09f6d325e554e65cd5ada44986182510e7e57e33
Linh Nguyen <linhnguyendeveloper@gmail.com>
2024-08-17 22:16:29 +0700

    Update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,17 @@
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <h1>Hello, Not found!</h1>;
+  return (
+    <section className="bg-white dark:bg-white">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-gray-900">Something's missing.</p>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-500">
+            Sorry, we can't find that page. You'll find lots to explore on the home page.{" "}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
